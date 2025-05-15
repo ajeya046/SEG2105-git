6f5c1d6316516e87acef56e6598662c8bf88f9ff
CodingCore83642 <146789870+CodingCore83642@users.noreply.github.com>
2025-05-15 09:19:53 -0400

    OutputManager

---
+++ UserAccount.java
@@ -0,0 +1,9 @@
+
+public class UserAccount {
+    String username;
+    String password;
+    public UserAccount(String inputtedUsername, String inputtedPassword) {
+        username = inputtedUsername;
+        password = inputtedPassword;
+    }
+}
